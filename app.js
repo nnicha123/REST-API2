d3f9bc1cfa3627e76aa524e59161355f0c9c4314
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-30 16:42:02 +0700

    try to add new mongodb proj

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+// // ROUTES
+// app.get('/',(req,res)=>{
+//     res.send('We are on home')
+// })
+// app.get('/posts',(req,res)=>{
+//     res.send('We are on posts')
+// })
+
+// Connect to db
+mongoose.connect('mongodb://localhost/newsuperTest')
+
+mongoose.connection.once('open',function(){
+    console.log('Connection has been made, now make fireworks')
+}).on('errpr',function(error){
+    console.log('COnnection error', error)
+})
+
+// // Listen
+// app.listen(4000)
